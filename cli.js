bee90f91b3cb3312fdb5f4da3792dda6e3a72b65
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-09-12 14:30:08 +0100

    More stuff

--- cli.js
+++ cli.js
@@ -1,3 +1,31 @@
 console.log("====================================");
 console.log("Welcome to the MeiliSearch Toolkit");
 console.log("====================================");
+
+const readline = require("readline")
+const config = require("./toolkitconfig.json")
+
+const rl =
+    readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+
+if (config.server.host == null) {
+
+}
+
+if (config.database.enabled) {
+    console.log("Connecting to database...");
+
+    console.log("====================================");
+}
+
+console.log("Options:");
+console.log("1. Index data");
+
+
+rl.question("What would you like to do? ", function(name) {
+    console.log("Hello " + name + "!")
+    rl.close()
+});
